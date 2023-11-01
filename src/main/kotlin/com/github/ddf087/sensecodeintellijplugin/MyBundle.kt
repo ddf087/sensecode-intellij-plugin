b5753c10910cbfd4e19f39ee81a44698c85f2d01
GitHub Action <action@github.com>
2023-11-01 06:44:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/ddf087/sensecodeintellijplugin/MyBundle.kt
+++ src/main/kotlin/com/github/ddf087/sensecodeintellijplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ddf087.sensecodeintellijplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/ddf087/sensecodeintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ddf087/sensecodeintellijplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.ddf087.sensecodeintellijplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ddf087.sensecodeintellijplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
